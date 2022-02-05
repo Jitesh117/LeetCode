19dad48c82c0d496eecd2111d74e2dd1d8c39023
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-02-05 21:01:23 +0530

    Time: 24 ms (5.31%), Space: 5.9 MB (24.53%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int i =1;
+        while(n>0)
+        {
+            i++;
+            n-=i;
+        }
+        return i-1;
+    }
+};
