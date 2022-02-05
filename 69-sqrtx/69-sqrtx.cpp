5453e9071ba3e08bf8dfa0ecfa8f38d18252732c
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-02-05 20:30:03 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.63%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+       long long s=0, e=x, ans, mid;   
+        while(s<=e){             
+            mid=(s+e)/2;
+            if(mid*mid==x) return mid;     
+            else if(mid*mid<x){             
+                s=mid+1;        
+                ans=mid;        
+            }
+            else e=mid-1;       
+        }
+        return ans;   
     }
 };
