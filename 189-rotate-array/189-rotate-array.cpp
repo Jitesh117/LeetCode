88b7dd8e912e10f2ab2ecdd70abbe6cd197a88e4
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-03-14 20:50:35 +0530

    Updated the Rotate array solution
    Implemented the reverse array algo from scratch

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -2,10 +2,21 @@ class Solution {
 public:
     void rotate(vector<int>& v, int k) {
        k = k%(v.size());
-        reverse(v.begin(),v.end());
-        reverse(v.begin(),v.begin()+k);
-        reverse(v.begin()+k,v.end());
+        reverse(v,0,v.size()-1);
+        reverse(v,k,v.size()-1);
+        reverse(v,0,k-1);
         
         
+    }
+    void reverse(vector<int>& v,int start, int end)
+    {
+        while(start<end)
+        {
+            int temp = v[start];
+            v[start] = v[end];
+            v[end] = temp;
+            end--;
+            start++;
+        }
     }
 };
