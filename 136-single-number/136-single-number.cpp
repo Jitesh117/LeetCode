95a883a940de1379738d3a27a71ebac7e3183c18
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-03-26 06:02:03 +0530

    Time: 19 ms (82.08%), Space: 16.9 MB (49.48%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
+        int ans  = 0;
         for(int i = 0;i<nums.size();i++)
         {
             ans = (nums[i]^ans);
