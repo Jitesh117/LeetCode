251f44ce1ac671d3d6b07a6b97d4f6d0ea3cb69c
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-03-27 08:14:14 +0530

    Time: 0 ms (100.00%), Space: 6 MB (55.23%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+       int t[n+1];
+        for(int i = 0;i<n+1;i++)
+            t[i] = 0;
+        t[1] = 1;
+        if(n>=2) 
+        {
+            t[2] = 2;
+            for(int i = 3;i<n+1;i++)
+                t[i] = t[i-1]+t[i-2];
+        }
+        return t[n];
+        
+    }
+};
