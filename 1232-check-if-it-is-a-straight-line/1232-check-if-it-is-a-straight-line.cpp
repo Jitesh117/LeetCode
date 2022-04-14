34ed4998da36bcc82cf06d2bb23428085d651b6c
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-04-14 09:54:10 +0530

    Time: 7 ms (93.54%), Space: 10 MB (78.60%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& c) {
+      int xdiff = c[1][0] - c[0][0];
+      int ydiff = c[1][1] - c[0][1];
+        
+        for(int i = 2;i<c.size();i++)
+        {
+            int x = c[i][0] - c[0][0];
+            int y = c[i][1] - c[0][1];
+            
+            if(x*ydiff !=y*xdiff)
+                return false;
+        }
+        
+        return true;
+    }
+};
