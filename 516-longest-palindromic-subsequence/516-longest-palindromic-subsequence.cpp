bfa18cbaaf663ce6067af74f9d287b94ea11c67d
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-04-15 06:19:25 +0530

    Time: 76 ms (86.03%), Space: 10.7 MB (79.70%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    string reverseStr(string& str)
-{
-    int n = str.length();
- 
-    for (int i = 0; i < n / 2; i++)
-        swap(str[i], str[n - i - 1]);
-    return str;
-}
-    int longestPalindromeSubseq(string x) {
-     return LCS(x,reverseStr(x));
-    }
-     int LCS(string text1, string text2) {
-        int n = text1.size(),m = text2.size();
-        int t[n+1][m+1];
-        
+    int longestPalindromeSubseq(string s) {
+        string t1 = s;
+        reverse(t1.begin(),t1.end());
+        int n = s.size();
+        int t[n+1][n+1];
         for(int i = 0;i<=n;i++)
-            for(int j = 0;j<=m;j++)
-                if(i == 0 || j == 0)
-                    t[i][j] = 0;
-        
-        for(int i = 1;i<=n;i++)
-            for(int j = 1;j<=m;j++)
+            for(int j = 0;j<=n;j++)
             {
-                if(text1[i-1] == text2[j-1])
+                if( i == 0 || j == 0)
+                    t[i][j] = 0;
+            }
+        for(int i = 1;i<=n;i++)
+            for(int j = 1;j<=n;j++)
+            {
+                if(s[i-1] == t1[j-1])
+                {
                     t[i][j] = 1+t[i-1][j-1];
+                }
                 else
                     t[i][j] = max(t[i-1][j],t[i][j-1]);
             }
-        return t[n][m];
+        return t[n][n];
     }
 };
