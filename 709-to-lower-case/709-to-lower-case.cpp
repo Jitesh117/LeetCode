2617437764107640ef5581524ca5bc0a4d48d0db
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-04-17 06:15:44 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (7.08%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     string toLowerCase(string s) {
         for(int i = 0;i<s.size();i++)
+            if(isupper(s[i]))
             s[i] = tolower(s[i]);
         return s;
     }
