bd4569ea5a01247878bb31ca395793b4329d952a
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-04-20 07:36:23 +0530

    Time: 2 ms (48.28%), Space: 6.2 MB (76.16%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string freqAlphabets(string s) {
+        int n = s.length(), i = 0;
+        string ans = "";
+        while(i < n)
+            if(i + 2 < n && s[i + 2] == '#') {
+                int ss = (s[i]-'0')*10 + (s[i+1]-'0');
+                ans += char(ss+'a'-1);
+                i += 3;
+            }
+            else {
+                ans += char(s[i]-'0'-1+'a');
+                i++;
+            }
+        
+        return ans;
+    }
+};
