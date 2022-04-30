f429dad1f92ae2babb9f5dd90f9a7615495b48c8
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-04-30 07:03:36 +0530

    Time: 135 ms (77.16%), Space: 11.3 MB (74.52%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        if(nums.size()<=1)
-            return false;
-        int total = 0;
+        int sum =0;
         for(int i = 0;i<nums.size();i++)
-            total+=nums[i];
-        if(total%2!=0)
+            sum+=nums[i];
+        if(sum%2!=0)
             return false;
+        return subset(nums,nums.size(),sum/2);
         
-        return solve(nums,total/2,nums.size());
-        
-    }       
-    bool solve(vector<int> nums,int sum,int n)
+    }
+    bool subset(vector<int> &nums,int n, int sum)
     {
         bool t[n+1][sum+1];
-        for(int i = 0;i<=n;i++)
-            for(int j = 0;j<sum+1;j++)
+        for(int i = 0 ;i<=n;i++)
+            for(int j = 0;j<=sum;j++)
             {
-                if(i == 0)
+                if( i == 0)
                     t[i][j] = false;
-                if(j == 0)
+                if( j == 0)
                     t[i][j] = true;
             }
-        for(int i = 1;i<n+1;i++)
-            for(int j = 1;j<sum+1;j++)
+        for(int  i = 1; i<=n;i++ )
+            for(int j = 1;j<=sum;j++)
             {
-                if(nums[i-1]<=j)
-                    t[i][j] = t[i-1][j - nums[i-1]] || t[i-1][j];
+                if( nums[i-1]<=j)
+                    t[i][j] = t[i-1][j] || t[i-1][j-nums[i-1]];
                 else
                     t[i][j] = t[i-1][j];
             }
-    return t[n][sum];
+        return t[n][sum];
     }
 };
