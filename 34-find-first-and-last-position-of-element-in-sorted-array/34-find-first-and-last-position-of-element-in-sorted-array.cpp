e69d93a0a74d31a2fab55b11016aa46fc7d0bac5
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-05-01 08:20:34 +0530

    Time: 12 ms (47.03%), Space: 13.7 MB (19.33%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,42 +2,46 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> v;
-        v.push_back(firstpos(nums,target));
-        v.push_back(endpos(nums,target));
+        v.push_back(first(nums,target));
+        v.push_back(last(nums,target));
         return v;
     }
-    int firstpos(vector<int>& v,int target)
-    {   int first = -1;
-        int beg = 0,last = v.size()-1;
-        while(beg<=last)
-        {   int mid = (beg+last)/2;
-            if(v[mid] == target)
+    int first(vector<int>& nums,int target)
+    {
+        int fpos=-1;
+        int beg = 0,end = nums.size()-1;
+        while(beg<=end)
+        {
+            int mid = beg + (end-beg)/2;
+            if(nums[mid] == target)
             {
-                first = mid;
-                last= mid-1;
+                fpos=mid;
+                end=mid-1;
             }
-            else if(v[mid]<target)
-                beg = mid+1;
+            else if(nums[mid]<target)
+                beg=mid+1;
             else
-                last = mid-1;
+                end = mid-1;
         }
-     return first;
+        return fpos;
     }
-    int endpos(vector<int>& v,int target)
-    {   int end = -1;
-        int beg = 0,last = v.size()-1;
-        while(beg<=last)
-        {   int mid = (beg+last)/2;
-            if(v[mid] == target)
+     int last(vector<int>& nums,int target)
+    {
+        int lpos=-1;
+        int beg = 0,end = nums.size()-1;
+        while(beg<=end)
+        {
+            int mid = beg + (end-beg)/2;
+            if(nums[mid] == target)
             {
-                end = mid;
-               beg = mid+1;
+                lpos=mid;
+                beg=mid+1;
             }
-            else if(v[mid]<target)
-                beg = mid+1;
+            else if(nums[mid]<target)
+                beg=mid+1;
             else
-                last = mid-1;
+                end = mid-1;
         }
-     return end;
+        return lpos;
     }
 };
