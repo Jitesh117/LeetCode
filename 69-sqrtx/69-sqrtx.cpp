6cf60436f9e2868549f42ad8ee1d4ca165b5f2a7
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-05-03 16:52:08 +0530

    Time: 3 ms (66.90%), Space: 5.9 MB (75.24%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-       long long s=0, e=x, ans, mid;   
-        while(s<=e){             
-            mid=(s+e)/2;
-            if(mid*mid==x) return mid;     
-            else if(mid*mid<x){             
-                s=mid+1;        
-                ans=mid;        
+    int mySqrt(long x) {
+        int beg = 0,end = x,ans;
+        while(beg<=end)
+        {
+            long mid = beg + (end-beg)/2;
+            if(mid*mid == x)
+                return mid;
+            else if(mid*mid<x)
+            {
+                ans=mid;
+                beg=mid+1;
             }
-            else e=mid-1;       
+            else
+                end=mid-1;
         }
-        return ans;   
+        return ans;
     }
 };
