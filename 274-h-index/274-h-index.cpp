539c7ca7c8b5a3222983d818ee4cbb039593bd2e
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-05-06 20:35:17 +0530

    Time: 3 ms (72.16%), Space: 8.6 MB (41.57%) - LeetHub

---
+++ 274-h-index/274-h-index.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int hIndex(vector<int>& c) {
+        sort(c.begin(),c.end());
+        return solve(c);
+        
+    } 
+    int solve(vector<int>& c) {
+        int n = c.size(),result = 0;
+        int beg = 0,end = n-1;
+        while(beg<=end)
+        {
+            int mid = beg + (end-beg)/2;
+            if(c[mid]>=n-mid)
+                end = mid-1;
+            else
+                beg = mid+1;
+        }
+        return n-beg;
+    }
+};
