537ae9ba05ee6b49ef299cc5c8d563022861a9b8
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-05-07 19:11:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (23.59%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,20 +4,19 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-      int bad;
       int beg = 1,end = n;
         while(beg<=end)
         {
             int mid = beg + (end-beg)/2;
             if(isBadVersion(mid))
             {
-                bad = mid;
+               
                 end = mid-1;
             }
             else
                 beg = mid+1;
             
         }
-        return bad;
+        return beg;
     }
 };
