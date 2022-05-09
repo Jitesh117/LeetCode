dcf17121e9e3cc2faadf285430e8472a59fddec4
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-05-09 18:15:00 +0530

    Time: 7 ms (48.30%), Space: 15.8 MB (15.46%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
-        int open =n,close = n;
+        string output;
         vector<string> result;
-        string output = "";
-        solve(open,close,output,result);
-        
+        solve(n,n,output,result);
         return result;
     }
     void solve(int open, int close, string output, vector<string> &result)
     {
+
         if(open == 0 && close == 0)
         {
             result.push_back(output);
@@ -18,13 +17,13 @@ public:
         if(open!=0)
         {
             string output1 = output;
-            output1.push_back('(');
+            output1+='(';
             solve(open-1,close,output1,result);
         }
         if(close>open)
         {
             string output2 = output;
-            output2.push_back(')');
+            output2+=')';
             solve(open,close-1,output2,result);
         }
     }
