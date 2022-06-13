fe4f3675a8be7f3dc8b4d35275db347b30040d5b
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-06-13 18:03:13 +0530

    Time: 5 ms (17.46%), Space: 6.8 MB (10.98%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        if(digits=="")
+        if(digits.size() == 0)
             return {};
-        
-        string op="";
-        vector<string> res;
-        solve(digits,op,res);
-        return res;
+        string output="";
+        vector<string>result;
+        solve(output,result,digits);
+        return result;
     }
-    
-    void solve(string digits,string op,vector<string> &res)
+    void solve(string output,vector<string>&result,string digits)
     {
-        vector<string> v={"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        
-        if(digits.size()==0) 
+         vector<string> v={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        if(digits.size() == 0)
         {
-            res.push_back(op);
+            result.push_back(output);
             return;
         }
-        
+       
         string s = v[digits[0]-'0'];
         digits.erase(digits.begin()+0);
-        for(int i = 0;i<s.size();i++)
-        {
-            solve(digits,op+s[i],res);
-        }
+        for(int i =0 ;i<s.size();i++)
+            solve(output+s[i],result,digits);
+        
     }
+        
 };
