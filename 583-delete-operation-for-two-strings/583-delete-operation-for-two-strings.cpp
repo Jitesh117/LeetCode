6bd9943a53167f6a3a2909a8c58675649effbfc1
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-06-14 06:14:31 +0530

    Time: 20 ms (72.27%), Space: 7.4 MB (85.76%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
+        int n = word1.size(),m=word2.size();
         int t[n+1][m+1];
         for(int i = 0;i<=n;i++)
             for(int j = 0;j<=m;j++)
-                if(j == 0 || i == 0)
+                if(i == 0 or j == 0)
                     t[i][j] = 0;
         for(int i = 1;i<=n;i++)
             for(int j = 1;j<=m;j++)
             {
-                if(word1[i-1] ==word2[j-1])
+                if(word1[i-1] == word2[j-1])
                     t[i][j] = 1+t[i-1][j-1];
                 else
                     t[i][j] = max(t[i-1][j],t[i][j-1]);
             }
-        return (n-t[n][m]) + (m-t[n][m]);
+        return n+m-2*t[n][m];
     }
 };
