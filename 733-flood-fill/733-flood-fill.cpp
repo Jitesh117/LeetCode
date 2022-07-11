ae5fc4157bc05a1f0f2bcaa52005e4c5c1b366e9
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-07-11 10:02:32 +0530

    Time: 16 ms (45.70%), Space: 14 MB (79.53%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     int m,n;
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        m=image.size();
+        m = image.size();
         n = image[0].size();
-        flood(image,sr,sc,image[sr][sc],color);
+        solve(image,sr,sc,color,image[sr][sc]);
         return image;
     }
-    void flood(vector<vector<int>>& image,int i,int j, int currcolor, int newcolor)
+    void solve(vector<vector<int>>& image,int i, int j, int color, int curcolor)
     {
-        if(i<0 || j<0 || i>=m || j>=n)
+        if(i<0 or j<0 or i>=m or j>=n)
             return;
-        if(image[i][j]!=currcolor)
+        if(image[i][j]!=curcolor)
             return;
-        if(image[i][j]==newcolor)
+        if(image[i][j]==color)
             return;
-        image[i][j]=newcolor;
-        flood(image,i+1,j,currcolor,newcolor);
-        flood(image,i-1,j,currcolor,newcolor);
-        flood(image,i,j-1,currcolor,newcolor);
-        flood(image,i,j+1,currcolor,newcolor);
+        image[i][j] = color;
+        
+        solve(image,i-1,j,color,curcolor);
+        solve(image,i+1,j,color,curcolor);
+        solve(image,i,j+1,color,curcolor);
+        solve(image,i,j-1,color,curcolor);
     }
 };
