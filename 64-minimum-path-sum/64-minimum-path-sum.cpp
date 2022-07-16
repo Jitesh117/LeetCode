bffd9f79a896ed5f452a61a580bc32a627e9eff2
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-07-16 17:52:15 +0530

    Time: 4 ms (98.47%), Space: 10.1 MB (52.10%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size(),m=grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,0));
-       return solve(0,0,grid,dp);
-    }
-    int solve(int i, int j, vector<vector<int>>&grid,vector<vector<int>>&dp)
-    {
-        if(i==grid.size()-1 and j == grid[i].size()-1)
-            return grid[i][j];
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
         
-        if(dp[i][j]!=0)
-            return dp[i][j];
-        
-        else if(i == grid.size()-1)
-            return dp[i][j] = grid[i][j]+solve(i,j+1,grid,dp);
-        
-        else if(j == grid[0].size()-1)
-            return dp[i][j] = grid[i][j]+solve(i+1,j,grid,dp);
-        
-        return dp[i][j] = grid[i][j]+min(solve(i+1,j,grid,dp),solve(i,j+1,grid,dp));
+        for(int i = 0;i<n;i++)
+            for(int j = 0;j<m;j++)
+            {
+                if(i == 0 and j == 0)
+                    dp[i][j] = grid[i][j];
+               else {int left = INT_MAX,up=INT_MAX;
+                if(i>0)
+                    up = grid[i][j]+dp[i-1][j];
+                if(j>0)
+                    left = grid[i][j] +dp[i][j-1];
+                dp[i][j] = min(up,left);}
+            }
+        return dp[n-1][m-1];
     }
 };
