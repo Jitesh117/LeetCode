6abe9b9507353062beff40178daff31aa1520680
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-07-16 11:29:06 +0530

    Time: 45 ms (16.17%), Space: 24.8 MB (56.20%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.cpp
+++ 337-house-robber-iii/337-house-robber-iii.cpp
@@ -9,21 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-     unordered_map<TreeNode*,int>umap;
-    int rob(TreeNode* root) {
-        if(root==NULL)return 0;
-        if(umap[root])return umap[root];
-        int left_max=0;
-        int right_max=0;
-        if(root->left){
-            left_max=rob(root->left->left)+rob(root->left->right);
+    unordered_map<TreeNode *, int> umap;
+    int rob(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+        if (umap[root])
+            return umap[root];
+        int left_max = 0;
+        int right_max = 0;
+        if (root->left)
+        {
+            left_max = rob(root->left->left) + rob(root->left->right);
         }
-       if(root->right){
-            right_max=rob(root->right->left)+rob(root->right->right);
-       }
-        
-        return umap[root]=max(root->val+left_max+right_max,rob(root->left)+rob(root->right));
+        if (root->right)
+        {
+            right_max = rob(root->right->left) + rob(root->right->right);
+        }
+
+        return umap[root] = max(root->val + left_max + right_max, rob(root->left) + rob(root->right));
     }
 };
