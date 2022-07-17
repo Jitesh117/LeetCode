8cdf37254d6806eb34fbb8f8a446c33292729a2a
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-07-17 11:02:20 +0530

    Time: 39 ms (8.06%), Space: 9.9 MB (82.69%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,38 +1,30 @@
-class Solution
-{
+class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>> &matrix)
-    {
-
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-        for (int j = 0; j < m; j++)
+    int minFallingPathSum(vector<vector<int>>& v) {
+        int n = v.size();
+        int result = INT_MAX;
+        int dp[n][n];
+        for(int j = 0;j<n;j++)
+            dp[n-1][j] = v[n-1][j];
+        
+        for(int i = n-2;i>=0;i--)
         {
-            dp[0][j] = matrix[0][j];
-        }
-
-        for (int i = 1; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
+            for(int j = 0;j<n;j++)
             {
-                int left = INT_MAX, right = INT_MAX;
-                int up = matrix[i][j] + dp[i - 1][j];
-                if (j - 1 >= 0)
-                    left = matrix[i][j] + dp[i - 1][j - 1];
-                if (j + 1 < m)
-                    right = matrix[i][j] + dp[i - 1][j + 1];
-                dp[i][j] = min(up, min(left, right));
+                int down=INT_MAX,left=INT_MAX,right=INT_MAX;
+                
+                down = dp[i+1][j];
+                if(j-1>=0)
+                    left = dp[i+1][j-1];
+                if(j+1<n)
+                    right = dp[i+1][j+1];
+                dp[i][j] = min(down,min(left,right))+v[i][j];
             }
         }
-
-        int mini = dp[n - 1][0];
-
-        for (int j = 1; j < m; j++)
-        {
-
-            mini = min(mini, dp[n - 1][j]);
-        }
-        return mini;
+        for(int j = 0;j<n;j++)
+            result = min(dp[0][j],result);
+        
+        return result;
     }
+
 };
