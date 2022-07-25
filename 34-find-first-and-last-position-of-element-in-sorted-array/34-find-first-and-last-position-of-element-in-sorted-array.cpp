6f4a7542b7ae53e6545ec9846d7bdd713eab6925
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-07-25 19:51:56 +0530

    Time: 23 ms (6.74%), Space: 13.6 MB (83.66%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
+    // vector<int>result;
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> v;
-        v.push_back(first(nums,target));
-        v.push_back(last(nums,target));
-        return v;
+        return {first(nums,target),last(nums,target)};
     }
-    int first(vector<int>& nums,int target)
+    int first(vector<int>&nums,int target)
     {
-        int fpos=-1;
         int beg = 0,end = nums.size()-1;
-        while(beg<=end)
-        {
-            int mid = beg + (end-beg)/2;
-            if(nums[mid] == target)
-            {
-                fpos=mid;
-                end=mid-1;
+        int fpos=-1;
+        while(beg<=end){
+            
+            int mid = beg+(end-beg)/2;
+            if(nums[mid] == target){
+                fpos = mid;
+                end = mid-1;
             }
             else if(nums[mid]<target)
-                beg=mid+1;
+                beg = mid+1;
             else
                 end = mid-1;
         }
         return fpos;
     }
-     int last(vector<int>& nums,int target)
-    {
-        int lpos=-1;
+    int last(vector<int>&nums,int target){
         int beg = 0,end = nums.size()-1;
-        while(beg<=end)
-        {
-            int mid = beg + (end-beg)/2;
-            if(nums[mid] == target)
-            {
-                lpos=mid;
-                beg=mid+1;
+        int lpos=-1;
+        while(beg<=end){
+            int mid = beg+(end-beg)/2;
+            if(nums[mid] == target){
+                lpos = mid;
+                beg = mid+1;
             }
             else if(nums[mid]<target)
-                beg=mid+1;
+                beg = mid+1;
             else
                 end = mid-1;
         }
