356ffa9dec3661f0ed42e3f337bc1d120d53f3ca
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-07-28 19:46:39 +0530

    Time: 26 ms (55.17%), Space: 12 MB (43.54%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
-    
-    int findTargetSumWays(vector<int>& nums, int target)
-     {
-         target = abs(target);
-         int n = nums.size();
-         int sum = 0;
-         for(int i = 0; i < n; i++)
-             sum += nums[i];
-         
-        int M = (sum + target)/2;
-        if(sum < target || (sum + target) % 2 != 0)
-            return 0;
-         return countSubsets(nums, n, M);
-     }
-     int countSubsets(vector<int>& nums, int n, int M)
+    int mod = (int)1e9 + 7;
+    int solve(int ind, int sum, vector<int> &nums, vector<vector<int>> &dp)
     {
-        int t[n + 1][M + 1];
-        
-       for(int i = 0; i <= n; i++)
+        if (ind == 0)
         {
-            for(int j = 0; j <= M; j++)
-            {
-                if(i == 0)
-                    t[i][j] = 0;
-                if(j == 0)
-                    t[i][j] = 1;
-            }
+            if (sum == 0 and nums[ind] == 0)
+                return 2;
+            if (sum == 0 or nums[ind] == sum)
+                return 1;
+            return 0;
         }
-        
-        
-        for(int i = 1; i <= n; i++)
-        {
-            for(int j = 0; j <= M; j++)
-            {
-                if(nums[i - 1] <= j)
-                 t[i][j] = t[i - 1][j - nums[i - 1]] + t[i - 1][j];
-                else
-                  t[i][j] = t[i - 1][j];
-            }
-        }
-        
-        return t[n][M];  
+        if (dp[ind][sum] != -1)
+            return dp[ind][sum];
+        int take = 0;
+        int nontake = solve(ind - 1, sum, nums, dp);
+        if (nums[ind] <= sum)
+            take = solve(ind - 1, sum - nums[ind], nums, dp);
+        return dp[ind][sum] = take + nontake;
     }
-    
+    int countPartitions(int n, int d, vector<int> &arr)
+    {
+        int totSum = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            totSum += arr[i];
+        }
+
+        // Checking for edge cases
+        if (totSum - d < 0)
+            return 0;
+        if ((totSum - d) % 2 == 1)
+            return 0;
+
+        int s2 = (totSum - d) / 2;
+
+        vector<vector<int>> dp(n, vector<int>(s2 + 1, -1));
+        return solve(n - 1, s2, arr, dp);
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return countPartitions(nums.size(),target,nums);
+    }
 };
