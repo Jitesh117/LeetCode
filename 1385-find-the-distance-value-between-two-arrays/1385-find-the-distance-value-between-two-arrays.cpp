7a128542313abe4352aa672b914ef3778fbda0ad
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-08-12 08:34:03 +0530

    Time: 7 ms (97.74%), Space: 13.2 MB (29.43%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-    
-    
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
-        
         sort(arr2.begin(),arr2.end());
-        
         int count = 0;
-        for(auto i:arr1){
-           if(bin(arr2,i,d)){
-               count++;
-           } 
+        for(auto it:arr1){
+            if(binsearch(arr2,it,d))
+                count++;
         }
-        
         return count;
     }
-    bool bin(vector<int>&arr,int target,int d){
-        
-        int low = 0, high = arr.size() - 1;
-        while(low <= high){
-            
-            int mid = low + (high - low)/2;
-            if(abs(arr[mid] - target) <= d){
-                return false;
-            }
-            else if(arr[mid] < target){
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
-            }
-            
+    bool binsearch(vector<int>&arr,int target,int d){
+        int beg = 0,end = arr.size()-1;
+        while(beg<=end){
+            int mid = beg + (end-beg)/2;
+            if(abs(arr[mid] - target)<=d) return false;
+            else if(arr[mid]<target) beg = mid+1;
+            else end = mid-1;
         }
         return true;
     }
