cdf570a10e2f6313b02d0b0e56bf373437b14341
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-10-07 19:18:22 +0530

    Time: 29 ms (16.96%), Space: 21.4 MB (38.36%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -11,15 +11,12 @@
  */
 class Solution {
 public:
-    // bool sum;
-    bool hasPathSum(TreeNode* root, int targetsum) {
-        if(!root )
-            return false;
-        int remain = targetsum - root->val;
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root) return false;
         
-        if(!root->left && !root->right)
-            if(remain == 0)
-                return true;
-       return(hasPathSum(root->left,remain) || hasPathSum(root->right,remain));
+        targetSum-=root->val;
+        if(!root->left and !root->right) 
+            if(targetSum == 0) return true;
+       return hasPathSum(root->left,targetSum) or hasPathSum(root->right,targetSum); 
     }
 };
