f837bc136651952a3f2d0bd4d1ace5f82d66b06d
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-10-17 07:04:48 +0530

    Added solution - LeetHub

---
+++ Pangram Strings - GFG/pangram-strings.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int isPanagram(string S)
+	{
+	    int check[26]={0};
+	    for(char it: S){
+	        it = tolower(it);
+	        check[it-'a']++;
+	    }
+	    for(int i = 0;i<26;i++)
+	        if(check[i] == 0) return false;
+	   return true;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	string tc;
+   	getline(cin, tc);
+   	t = stoi(tc);
+   	while(t--)
+   	{
+   		string s;
+   		getline(cin, s);
+
+   	    Solution ob;
+
+   		cout << ob.isPanagram(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
