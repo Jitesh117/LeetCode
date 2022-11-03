f906ba1800a27b8ac3865080704f0167c5d4475e
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-11-03 07:09:40 +0530

    Time: 934 ms (36.37%), Space: 167.9 MB (81.81%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+     unordered_map<string,int> umap;
+        int count=0;
+        for(string word:words)
+        {
+           umap[word]++;
+        }
+        //unordered_set<string> s;
+        bool flag=false;
+        for(string word:words)
+        {
+            if(umap[word])
+            {
+                string t="";
+                t+=word[1];
+                t+=word[0];
+                if(t==word)
+                    count+=umap[t]/2;
+                else count+=min(umap[t],umap[word]);
+                //s.insert(t);
+                //s.insert(word);
+                if(umap[t]%2==1 && t[0]==t[1])
+                    flag=true;
+                umap[word] = 0;
+                umap[t] = 0;
+            }
+        }
+        count*=4;
+        if(flag) count+=2;
+        return count;   
+    }
+};
