1e638ae6d182bf74fb9a73e3566fcb7fbbe063eb
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-12-23 18:08:28 +0530

    Time: 40 ms (88.05%), Space: 19.3 MB (13.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
+        int result = 0;
         int n = grid.size();
         int m = grid[0].size();
-        int count = 0;
-        vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<vector<int>> vis(n,vector<int>(m,0)); 
         for(int i = 0;i<n;i++)
-            for(int j = 0;j<m;j++){
-                if(vis[i][j]!=1 and grid[i][j] == '1'){
-                    count++;
-                    bfs(i,j,vis,grid);
+            for(int j = 0;j<m;j++)
+                if(!vis[i][j] and grid[i][j] == '1') {
+                    result++;
+                    bfs(i,j,grid,vis);
                 }
-            }
-        return count;
+        return result;
     }
-    void bfs(int i, int j, vector<vector<int>> &vis, vector<vector<char>> &grid)
-    {
-        vis[i][j] = 1;
+    void bfs(int i, int j, vector<vector<char>>&grid, vector<vector<int>>&vis){
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<int, int>> q;
-        q.push({i, j});
-
-        while (q.size())
-        {
+        
+        vis[i][j] = 1;
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        while(q.size()){
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
-
             for (int delrow = -1; delrow <= 1; delrow++)
                 for (int delcol = -1; delcol <= 1; delcol++)
                 {
                     int nrow = row + delrow;
                     int ncol = col + delcol;
-                    if((delrow==-1&&delcol==-1) || (delrow==-1&&delcol==1) || (delrow==1&&delcol==-1) || (delrow==1&&delcol==1)) continue;
+                    if((delrow==-1 and delcol==-1) or (delrow==-1 and delcol==1) or (delrow==1 and delcol==-1) or (delrow==1 and delcol==1)) continue;
                     
                     if (nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and grid[nrow][ncol] == '1' and !vis[nrow][ncol])
                     {
