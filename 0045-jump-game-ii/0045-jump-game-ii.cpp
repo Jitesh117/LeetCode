e45409ad1f21ace0542b8dce31ebe9bc51277dc7
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-12-26 10:21:48 +0530

    Time: 485 ms (35.60%), Space: 17.2 MB (42.96%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -10,13 +10,4 @@ public:
         return dp[0];
         
     }
-    int solve(int i, vector<int>&nums, vector<int>&dp){
-        if(i >= nums.size()-1)
-            return 0;
-        if(dp[i] != 1e9) return dp[i];
-        int result = 1e9;
-       for(int j = 1;j<=nums[i];j++)
-          result = min(result,1 + solve(i+j,nums,dp)); 
-        return dp[i] = result;
-    }
 };
