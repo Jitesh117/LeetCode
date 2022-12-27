968a0ea9b2181f331c3288822b78de41ecf1e5c6
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-12-27 21:06:30 +0530

    Time: 372 ms (13.32%), Space: 74.9 MB (8.21%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,59 +1,31 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid)
-    {
-        
-        int n = grid.size();
-        vector<vector<bool>> visited(n, vector<bool> (n, false));
-        queue<pair<int, int>> q;
-        int result = 0;
-        int npush;
-        
-        if(grid[0][0] == 0)
-        {
-            q.push({0, 0});
-            visited[0][0] = true;
-        }
-                
-        while(!q.empty())
-        {
-            
-            npush = q.size();
-            result++;
-            
-            for(int cnt = 0; cnt < npush; cnt++)
-            {
-                
-                pair<int, int> node = q.front();
-                q.pop();
-            
-                int i = node.first, j = node.second;
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int res = 1;
+        int row = grid.size();
+        if (row == 0) return -1;
+        int col = grid[0].size();
+        if (grid[0][0] != 0 | grid[row-1][col-1] != 0) return -1;
 
-                if(i==n-1 and j==n-1) 
-                    return result;
-
-                for(int k = i - 1; k <= i + 1 ; k++){
-                    for(int l = j - 1; l <= j + 1; l++)
-                    {
-                        if(isValid(grid, k, l, n, visited))
-                        {
-                            q.push({k, l});
-                            visited[k][l] = true;
-                        }
-                    }
-                }                
-                
-            }            
+        queue<pair<int, int>> queue;
+        queue.push(make_pair(0,0));
+        vector<vector<int>> directions = {{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
+        grid[0][0] = 1;
+        while(!queue.empty()){
+            auto curr = queue.front();
+            int x = curr.first, y = curr.second;
+            if( x == row -1 && y == col -1) return grid[x][y];
             
+            for(auto direction : directions){
+                int nx = x + direction[0];
+                int ny = y + direction[1];
+                if(nx >= 0 && nx < row && ny >= 0 && ny < col && grid[nx][ny] == 0){
+                    queue.push(make_pair(nx,ny));
+                    grid[nx][ny] = grid[x][y] + 1;
+                }
+            }
+            queue.pop();
         }
-        
         return -1;
-        
-    }
-    
-    bool isValid(vector<vector<int>>& grid, int i, int j, int n, vector<vector<bool>>& visited){
-        
-        return (i>=0 and i<n and j>=0 and j<n and grid[i][j]==0 and !visited[i][j]);
-        
     }
 };
