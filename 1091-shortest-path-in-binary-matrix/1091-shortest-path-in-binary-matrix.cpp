c81d9a24dcf6db0d34bcce21e424237b337426f3
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-12-27 21:07:40 +0530

    Time: 382 ms (12.76%), Space: 75 MB (6.57%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -7,21 +7,21 @@ public:
         int col = grid[0].size();
         if (grid[0][0] != 0 | grid[row-1][col-1] != 0) return -1;
 
-        queue<pair<int, int>> queue;
-        queue.push(make_pair(0,0));
+        queue<pair<int, int>> q;
+        q.push(make_pair(0,0));
         vector<vector<int>> directions = {{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
         grid[0][0] = 1;
-        while(!queue.empty()){
-            auto curr = queue.front();
+        while(q.size()){
+            auto curr = q.front();
             int x = curr.first, y = curr.second;
-            queue.pop();
+            q.pop();
             if( x == row -1 && y == col -1) return grid[x][y];
             
             for(auto direction : directions){
                 int nx = x + direction[0];
                 int ny = y + direction[1];
                 if(nx >= 0 && nx < row && ny >= 0 && ny < col && grid[nx][ny] == 0){
-                    queue.push(make_pair(nx,ny));
+                    q.push(make_pair(nx,ny));
                     grid[nx][ny] = grid[x][y] + 1;
                 }
             }
