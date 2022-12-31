d9819687a309747a83cdf3ef3c638ef211eef6a9
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-12-31 08:37:07 +0530

    Time: 2 ms (87.27%), Space: 7.1 MB (64.25%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -1,16 +1,5 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>> &grid, int i, int j, int stepsTaken, int requiredSteps){
-        if (i < 0 or j < 0 or i >= grid.size() or j >= grid[0].size() or grid[i][j] == -1)
-            return 0;
-        if (grid[i][j] == 2)
-            return stepsTaken == requiredSteps;
-        grid[i][j] = -1;
-        int paths = dfs(grid, i + 1, j, stepsTaken + 1, requiredSteps) + dfs(grid, i - 1, j, stepsTaken + 1, requiredSteps) +
-                    dfs(grid, i, j + 1, stepsTaken + 1, requiredSteps) + dfs(grid, i, j - 1, stepsTaken + 1, requiredSteps);
-        grid[i][j] = 0;
-        return paths;
-}
 int uniquePathsIII(vector<vector<int>> &grid){
     int starti = 0, startj = 0, requiredSteps = 0;
     for (auto i = 0; i < grid.size(); ++i)
@@ -22,5 +11,16 @@ int uniquePathsIII(vector<vector<int>> &grid){
                 ++requiredSteps;
         }
     return dfs(grid, starti, startj, 1, requiredSteps);
+}
+    int dfs(vector<vector<int>> &grid, int i, int j, int stepsTaken, int requiredSteps){
+        if (i < 0 or j < 0 or i >= grid.size() or j >= grid[0].size() or grid[i][j] == -1)
+            return 0;
+        if (grid[i][j] == 2)
+            return stepsTaken == requiredSteps;
+        grid[i][j] = -1;
+        int paths = dfs(grid, i + 1, j, stepsTaken + 1, requiredSteps) + dfs(grid, i - 1, j, stepsTaken + 1, requiredSteps) +
+                    dfs(grid, i, j + 1, stepsTaken + 1, requiredSteps) + dfs(grid, i, j - 1, stepsTaken + 1, requiredSteps);
+        grid[i][j] = 0;
+        return paths;
 }
 };
