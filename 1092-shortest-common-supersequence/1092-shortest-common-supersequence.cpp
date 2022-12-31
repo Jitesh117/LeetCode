63d974bab57ac130b2bd803c4d4879a067438195
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2022-12-31 10:32:28 +0530

    Time: 14 ms (89.92%), Space: 10.7 MB (92.75%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,53 +1,47 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        return SCS(str1,str2,str1.size(),str2.size());
+        int n = str1.size();
+        int m = str2.size();
+        int dp[n + 1][m + 1]; 
+        for (int i = 0; i <= n; i++)
+            for (int j = 0; j <= m; j++)
+                if (i == 0 or j == 0)
+                    dp[i][j] = 0;
+
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= m; j++)
+            {
+                if (str1[i - 1] == str2[j - 1]) 
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else 
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        int i = n, j = m;
+        string result = "";
+        while (i > 0 && j > 0) {
+            if (str1[i - 1] == str2[j - 1]) {  
+                result += str1[i - 1]; 
+                i--, j--; 
+            }
+            else if (dp[i][j - 1] > dp[i - 1][j]) {
+                result += str2[j - 1]; 
+                j--;
+            }
+            else {
+                result += str1[i - 1]; 
+                i--;
+            }
+        }
+        while (i > 0) {
+            result += str1[i - 1];
+            i--;
+        }
+        while (j > 0) {
+            result += str2[j - 1]; 
+            j--;
+        }
+        reverse(result.begin(), result.end());
+        return result;
     }
-    string SCS(string X, string Y, int n, int m) {
-	int dp[n + 1][m + 1]; 
-
-	
-	for (int i = 0; i <= n; i++)
-		for (int j = 0; j <= m; j++)
-			if (i == 0 || j == 0)
-				dp[i][j] = 0;
-
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= m; j++)
-			if (X[i - 1] == Y[j - 1]) 
-				dp[i][j] = 1 + dp[i - 1][j - 1];
-			else 
-				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-
-	int i = n, j = m;
-	string scs = "";
-	while (i > 0 && j > 0) {
-		if (X[i - 1] == Y[j - 1]) {  
-			scs += X[i - 1]; 
-			i--, j--; 
-		}
-		else if (dp[i][j - 1] > dp[i - 1][j]) {
-			scs += Y[j - 1]; 
-			j--;
-		}
-		else {
-			scs += X[i - 1]; 
-			i--;
-		}
-	}
-
-	while (i > 0) {
-		scs += X[i - 1];
-		i--;
-	}
-
-	while (j > 0) {
-		scs += Y[j - 1]; 
-		j--;
-	}
-
-	reverse(scs.begin(), scs.end());
-
-	return scs;
-}
 };
