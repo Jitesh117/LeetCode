d726f273891d4d745d347a9ce77ef45d41404052
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-01-01 07:41:58 +0530

    Time: 31 ms (90.32%), Space: 27.2 MB (37.85%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -3,23 +3,23 @@ public:
     bool isMatch(string text, string pattern) {
         int n = pattern.size();
         int m = text.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,pattern,text,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return solve(n,m,pattern,text,dp);
     }
     int solve(int i, int j, string &pattern, string &text,vector<vector<int>>&dp){
-        if(i<0 and j<0) return true;
-        if(i<0 and j>=0) return false;
+        if(i==0 and j==0) return true;
+        if(i==0 and j>0) return false;
         
-        if(j<0 and i>=0){
-            for(int ii = 0;ii<=i;ii++)
+        if(j==0 and i>0){
+            for(int ii = 0;ii<i;ii++)
                 if(pattern[ii] != '*') return false;
             return true;
         }
         if(dp[i][j] != -1) return dp[i][j];
         
-        if(pattern[i] == text[j] or pattern[i] == '?')
+        if(pattern[i-1] == text[j-1] or pattern[i-1] == '?')
             return dp[i][j] = solve(i-1,j-1,pattern,text,dp);
-        if(pattern[i] == '*'){
+        if(pattern[i-1] == '*'){
             return dp[i][j] = solve(i-1,j,pattern,text,dp) or solve(i,j-1,pattern,text,dp);
         }
         return dp[i][j] = false;
