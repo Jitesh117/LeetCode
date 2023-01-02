f2ce982db52581222dca57c37bf2de0eeb1b7988
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-01-02 11:21:00 +0530

    Time: 268 ms (20.87%), Space: 112.4 MB (5.14%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    unordered_map<int,int>m;
-    int dp[7001][2];
+    unordered_map<int,int>forbid;
     int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
-        
-        for(auto i:forbidden)
-            m[i] = 1;
-        
-        memset(dp,-1,sizeof(dp));
-        int result = solve(0,0,a,b,x);
-        return (result > 1e9)? -1:result;
+        vector<vector<int>>dp(7001,vector<int>(2,-1));
+        for(auto &i:forbidden)
+            forbid[i] = 1;
+        int result = solve(0,0,a,b,x,dp);
+        if(result >= 1e9) return -1;
+        return result;
     }
-    int solve(int i,bool back,int &a,int &b,int &x)
-    {
-        if(i == x)return 0;
-        if( i < 0 || m[i] || i>6000 )
-            return 1e9;
+    int solve(int i, bool back,int &a, int &b, int &x,vector<vector<int>>&dp){
+        if(i == x) return 0;
+        if(i<0 or forbid[i] or i>6000) return 1e9;
+        if(dp[i][back] != -1) return dp[i][back];
         
-        
-        if(dp[i][back]!=-1) return dp[i][back];
-        // either go forward 
-        dp[i][back] = 1 + solve(i+a,0,a,b,x);
-        //go backward but cannot go consecutively 2 times
+        dp[i][back] = 1 + solve(i+a,0,a,b,x,dp);
         if(!back)
-        {
-            dp[i][back] = min(dp[i][back],1 + solve(i-b,1,a,b,x));
-        }
-      
+            dp[i][back] = min(dp[i][back],1 + solve(i-b,1,a,b,x,dp));
         return dp[i][back];
     }
 };
