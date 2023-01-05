ad8301ef3e5ee09ae3588b961c4c52a7b70b5e92
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-01-05 18:44:41 +0530

    Time: 32 ms (55.08%), Space: 15.9 MB (27.70%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -4,25 +4,22 @@ public:
         // constructing adjacency matrix
 		vector<vector<pair<int,int>>> graph(n);
         for(auto edge: redEdges)
-            graph[edge[0]].push_back({edge[1],0}); //red edges are denoted by 0 
+            graph[edge[0]].push_back({edge[1],0}); 
         for(auto edge: blueEdges)
-            graph[edge[0]].push_back({edge[1],1}); // blue edges are denoted by 1
+            graph[edge[0]].push_back({edge[1],1}); 
         vector<int> dist(n,-1); 
         
         queue<vector<int>> q;
         q.push(vector<int>{0,0,-1});
         
-        while(!q.empty()) {
+        while(q.size()) {
             auto front = q.front();
             q.pop();
             dist[front[0]] = dist[front[0]] != -1 ? dist[front[0]] : front[1];
             
             for(auto &adj : graph[front[0]]) {
-				//Push the node to the queue only if the next edge color is different from the pervious edge color and also if we are visiting the edge
-				//for the first time.
                 if(front[2] != adj.second && adj.first!= -1) {
                     q.push(vector<int>{adj.first, front[1] + 1, adj.second});
-					//Update the value in the adjacency matrix to -1 to denote that the node has already been visited.
                     adj.first = -1;
                 }
             }
