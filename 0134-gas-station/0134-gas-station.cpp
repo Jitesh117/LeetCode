b7fe26ec1c96649d38b6a6be5dd83382745fd759
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-01-07 08:28:40 +0530

    Time: 92 ms (77.98%), Space: 69.5 MB (55.78%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int tot_gas = 0,tot_cost = 0;
+        int cur_gas = 0;
+        int start_point = 0;
+        for(int i = 0;i<n;i++) {
+            tot_gas+=gas[i];
+            tot_cost+=cost[i];
+            cur_gas+=gas[i]-cost[i];
+            if(cur_gas<0) {
+                start_point=i+1;
+                cur_gas=0; 
+            }
+        }
+        if(tot_gas<tot_cost) return -1;
+        return start_point;
+    }
+};
