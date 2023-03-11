c43a2ee41495e82d03c66b5010cb21269aad9771
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-03-11 23:25:41 +0530

    Time: 36 ms (30.08%), Space: 28.3 MB (92.35%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -19,29 +19,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-    TreeNode* constructBST(ListNode *leftHead, ListNode *rightHead) {
-        if (leftHead == rightHead)
-            return nullptr;
-        ListNode *slow = leftHead, *fast = leftHead;
-        while (fast != rightHead && fast -> next != rightHead) {
-            slow = slow -> next;
-            fast = fast -> next -> next;
-        }
-        TreeNode *root = new TreeNode(slow -> val);
-        root -> left = constructBST(leftHead, slow);
-        root -> right = constructBST(slow -> next, rightHead);
-        return root;
-    }
     TreeNode* sortedListToBST(ListNode* head) {
-        if (head == nullptr)
-            return nullptr;
+        if(!head) return NULL;
         if (head -> next == nullptr) {
             TreeNode *root = new TreeNode(head -> val);
             return root;
         }
-        return constructBST(head, nullptr);
+        return binarySearchTree(head,NULL);
+    }
+    TreeNode* binarySearchTree(ListNode* left, ListNode* right){
+        if(left == right) return NULL;
+        ListNode* slow = left, *fast = left;
+        while(fast != right and fast->next != right){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        TreeNode *root = new TreeNode(slow -> val);
+        root->left = binarySearchTree(left,slow);
+        root->right = binarySearchTree(slow->next,right);
+        
+        return root;
     }
 };
