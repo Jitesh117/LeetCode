7faffb48eb412e511efa3b468ae917ec0e143db6
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-05-15 06:17:38 +0530

    Time: 58 ms (66.06%), Space: 41.5 MB (79.38%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} millis
+ */
+
+async function sleep(millis) {
+    const promise = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve()
+        },millis);
+    })
+    return promise;
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
