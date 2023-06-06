6465463e680bc22641d0af1cd3ca8c31a2f95cb0
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-06-06 21:37:02 +0530

    Time: 5 ms (30.87%) | Memory: 6.2 MB (75.16%) - LeetSync

---
+++ decode-ways/decode-ways.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int>dp(n,-1);
+        return s.empty() ? 0: solve(0,s, dp);    
+    }
+    int solve(int i, string& s, vector<int>&dp) {
+        int n = s.size();
+        if(i == n) return 1;
+        if(dp[i] != -1) return dp[i];
+        if(s[i] == '0') return 0; 
+        int nonTake = solve(i+1,s, dp);
+        int take = 0;
+        if( i < n-1 and (s[i]=='1' or (s[i]=='2' and s[i+1]<'7'))) 
+            take = solve(i + 2, s, dp);
+        // res += solve(p+2,s);
+        return dp[i] = take + nonTake;
+    }
+};
