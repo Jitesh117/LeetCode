8aed0afacf80aa5f1eb6c4b93012747e88de99ef
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-06-06 21:56:32 +0530

    Time: 20 ms (41.47%) | Memory: 11.1 MB (54.44%) - LeetSync

---
+++ word-break/word-break.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+     bool wordBreak(string s, vector<string>& wordDict) {
+        int n =s.size();
+        
+        set<string> dict;
+        for(auto w: wordDict)
+            dict.insert(w);
+        
+        vector<bool> dp(n+1,false);
+        dp[0] = true; 
+        
+        for(int i=0;i<n;i++){
+            for(int j=i;j>=0;j--){
+                string curr = s.substr(j,i-j+1);
+                
+                if(dict.find(curr)!=dict.end())
+                    dp[i+1] = dp[i+1] || dp[j]; 
+                
+                if(dp[i+1])
+                    break;
+            }
+        }
+        
+        return dp[n];
+    }
+};
