b17bf066fe230836d2b73ca7f85c2845aa71ec33
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-06-06 21:21:08 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (39.49%) - LeetSync

---
+++ unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int>dp = vector<int>(20,0);
+    int numTrees(int n) {
+        if(n <= 1) return 1;
+        int result = 0;
+        if(dp[n]) return dp[n];
+        for(int i = 1; i <= n; i++) 
+            result += numTrees(i-1) * numTrees(n-i);
+        return dp[n] = result;  
+    }
+};
