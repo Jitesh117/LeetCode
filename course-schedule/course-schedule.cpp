9ad3e6534bc9c2e35951f48692e7b03bcf0b24bf
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-06-28 18:39:22 +0530

    Time: 23 ms (71.71%) | Memory: 13.9 MB (46.56%) - LeetSync

--- course-schedule/course-schedule.cpp
+++ course-schedule/course-schedule.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = numCourses;
+       int n = numCourses;
        vector<vector<int>> adj(n);
-       for(auto it: prerequisites){
-           adj[it[1]].push_back(it[0]);
-       }
-       vector<int>vis(n, 0);
-       vector<int>dfsVis(n, 0);
+       vector<int>vis(n,0), dfsVis(n,0);
+       for(int i = 0; i < prerequisites.size(); i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+       } 
        for(int i = 0; i < n; i++){
-           if(checkCycle(i, adj, vis, dfsVis)) return false;
+           if(!vis[i]){
+               if(checkCycle(i, adj, dfsVis, vis)) return false;
+           }
        }
        return true;
     }
-
-    bool checkCycle(int node, vector<vector<int>>&adj, vector<int>&vis, vector<int>&dfsVis){
-        vis[node] = 1;
-        dfsVis[node] = 1;
-        for(auto it: adj[node]){
-            if(!vis[it]){
-                if(checkCycle(it, adj, vis, dfsVis)) return true;
-            }
-            else if(dfsVis[it]) return true;
-        }
-        dfsVis[node] = 0;
-        return false;
+    bool checkCycle(int i, vector<vector<int>>&adj, vector<int>& dfsVis, vector<int>&vis){
+       vis[i] = 1;
+       dfsVis[i] = 1;
+       for(auto it: adj[i]){
+           if(!vis[it]){
+               if(checkCycle(it, adj, dfsVis, vis)) return true;
+           }
+           else if(dfsVis[it]) return true;
+       }
+       dfsVis[i] = 0;
+       return false;
     }
 };
