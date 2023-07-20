b413e081eb7d8bd83671361fd6ab5fb811ba6e3e
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-07-20 21:38:03 +0530

    Time: 4 ms (61.81%) | Memory: 8.4 MB (19.07%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+     unordered_map<int,int> count;
+     unordered_map<int,int> freq;
+        for(int i : arr)
+            count[i]++;
+       for(auto it : count) {
+           if(freq[it.second] != 0)
+               return false;
+           freq[it.second] = 1;
+       } 
+        return true;
+    }
+};
