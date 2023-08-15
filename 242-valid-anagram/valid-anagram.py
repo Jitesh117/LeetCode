70912d2209c76d31347313f1cfc2937afcfeb280
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-08-15 16:07:11 +0530

    Time: 68 ms (40.33%) | Memory: 17.5 MB (21.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,17 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # one = {}
-        # two = {}
-        # for i in s:
-        #     if i in one:
-        #         one[i] += 1
-        #     else
-        #         one[i] = 1
-        # for i in t:
-        #     if i in two:
-        #         two[i] += 1
-        #     else
-        #         two[i] = 1
         if sorted(s) == sorted(t):
             return True
         return False
