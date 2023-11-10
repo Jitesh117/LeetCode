5819da4cd00ee27d713a6ae0c68ea2f7af8957a1
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-11-10 09:46:43 +0530

    Time: 345 ms (79.09%) | Memory: 122.7 MB (56.36%) - LeetSync

--- 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.cpp
+++ 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    vector<int> restoreArray(vector<vector<int>>& v) {
-        unordered_map<int,vector<int>>map;
+    vector<int> restoreArray(vector<vector<int>>& pairs) {
         vector<int>result;
-        int first_element = 0;
-        for(int i = 0; i < v.size(); i++){
-            map[v[i][0]].push_back(v[i][1]);
-            map[v[i][1]].push_back(v[i][0]);
+        unordered_map<int,vector<int>>map;
+        for(auto it: pairs){
+            map[it[0]].push_back(it[1]);
+            map[it[1]].push_back(it[0]);
         }
-        for(auto p: map){
-            if(p.second.size() == 1){
-                result.push_back(p.first);
-                result.push_back(p.second[0]);
+        for(auto it: map){
+            if(it.second.size() == 1){
+                result.push_back(it.first);
+                result.push_back(it.second[0]);
                 break;
             }
-        } 
-       while (result.size() < v.size() + 1) {
-        auto tail = result.back(), prev = result[result.size() - 2];
-        auto next = map[tail];
-        if (next[0] != prev)
-            result.push_back(next[0]);
-        else
-            result.push_back(next[1]);
         }
-        return result; 
+        while(result.size() < pairs.size() + 1){
+            int last = result.back();
+            int prev = result[result.size() - 2];
+            auto next = map[last];
+            if(prev != next[0]) result.push_back(next[0]);
+            else result.push_back(next[1]);
+        }
+        return result;
     }
 };
