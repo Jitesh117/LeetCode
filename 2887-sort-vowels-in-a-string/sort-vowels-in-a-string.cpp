49b1e1340f8d0efb71c85d010cc12b8463cafc68
Jitesh Kumar Sahoo <77315098+Jitesh117@users.noreply.github.com>
2023-11-13 08:27:49 +0530

    Time: 44 ms (42.53%) | Memory: 12.4 MB (72.42%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        vector<char> v;
-        unordered_map<int,int> pos;
-        for(int i = 0;i < s.size(); i++){
-            if(vowel(s[i])){
-                 v.push_back(s[i]);
-                pos[i]++;
-            }
-        }
+        vector<char>v;
+        for(char c : s){
+            if(isVowel(c)) v.push_back(c);
+        }    
         sort(v.begin(), v.end());
         int j = 0;
-        for(int i = 0; i< s.size(); i++){
-            if(pos[i]) s[i] = v[j++];
+        for(int i = 0; i < s.size(); i++){
+            if(isVowel(s[i])) swap(s[i], v[j++]);
         }
         return s;
-
     }
-    bool vowel(char x)
-    {
-    if (x == 'a' || x == 'e' || x == 'i' || 
-        x == 'o' || x == 'u' || x == 'A' || 
-        x == 'E' || x == 'I' || x == 'O' || x == 'U')
-       return true;
+    
+    bool isVowel(char &x){
+        if (x == 'a' || x == 'e' || x == 'i' || 
+            x == 'o' || x == 'u' || x == 'A' || 
+            x == 'E' || x == 'I' || x == 'O' || x == 'U')
+                return true;
         return false;
 }
 };
